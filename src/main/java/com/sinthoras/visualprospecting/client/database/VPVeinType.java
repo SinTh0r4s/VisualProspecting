1a028f0977efc35ccb7793933968f9b413a143c6
Johann Bernhardt <johann.bernhardt@tum.de>
2021-09-13 07:29:08 +0200

    implement vein matching

--- src/main/java/com/sinthoras/visualprospecting/client/database/VPVeinType.java
+++ src/main/java/com/sinthoras/visualprospecting/client/database/VPVeinType.java
@@ -1,5 +1,7 @@
 package com.sinthoras.visualprospecting.client.database;
 
+import java.util.HashSet;
+
 public class VPVeinType {
 
     public final String name;
@@ -7,6 +9,7 @@ public class VPVeinType {
     public final short secondaryOreMeta;
     public final short inBetweenOreMeta;
     public final short sporadicOreMeta;
+    private final HashSet<Short> oresAsHashSet;
 
     public VPVeinType(String name, short primaryOreMeta, short secondaryOreMeta, short inBetweenOreMeta, short sporadicOreMeta)
     {
@@ -15,5 +18,14 @@ public class VPVeinType {
         this.secondaryOreMeta = secondaryOreMeta;
         this.inBetweenOreMeta = inBetweenOreMeta;
         this.sporadicOreMeta = sporadicOreMeta;
+        oresAsHashSet = new HashSet<>();
+        oresAsHashSet.add(primaryOreMeta);
+        oresAsHashSet.add(secondaryOreMeta);
+        oresAsHashSet.add(inBetweenOreMeta);
+        oresAsHashSet.add(sporadicOreMeta);
+    }
+
+    public boolean matches(HashSet<Short> foundOres) {
+        return oresAsHashSet.equals(foundOres);
     }
 }
