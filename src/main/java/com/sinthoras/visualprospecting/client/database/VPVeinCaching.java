ceec315d0d1cb204a9de60618f84e2a06a8f22de
Johann Bernhardt <johann.bernhardt@tum.de>
2021-09-13 07:28:48 +0200

    Add a set of ores that can be in veins larger 3x3 chunks

--- src/main/java/com/sinthoras/visualprospecting/client/database/VPVeinCaching.java
+++ src/main/java/com/sinthoras/visualprospecting/client/database/VPVeinCaching.java
@@ -4,6 +4,7 @@ import com.sinthoras.visualprospecting.VP;
 import gregtech.common.GT_Worldgen_GT_Ore_Layer;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static com.sinthoras.visualprospecting.VPUtils.isBartworksInstalled;
@@ -12,6 +13,7 @@ import static com.sinthoras.visualprospecting.client.VPReflection.*;
 public class VPVeinCaching implements Runnable {
 
     public static List<VPVeinType> veinTypes;
+    public static HashSet<Short> largeVeinOres;
 
     // BartWorks initializes veins in FML preInit
     // GalacticGreg initializes veins in FML postInit, but only copies all base game veins to make them available on all planets
@@ -19,8 +21,16 @@ public class VPVeinCaching implements Runnable {
     // Therefore, this method must be called after GregTech postInit
     public void run() {
         veinTypes = new ArrayList<>();
+        largeVeinOres = new HashSet<>();
         for(GT_Worldgen_GT_Ore_Layer vein : GT_Worldgen_GT_Ore_Layer.sList) {
             veinTypes.add(new VPVeinType(vein.mWorldGenName, vein.mPrimaryMeta, vein.mSecondaryMeta, vein.mBetweenMeta, vein.mSporadicMeta));
+            if(vein.mSize > 16) {
+                // These veins can be larger then 3x3 chunks. For these ores we need to check multiple possible locations
+                largeVeinOres.add(vein.mPrimaryMeta);
+                largeVeinOres.add(vein.mSecondaryMeta);
+                largeVeinOres.add(vein.mBetweenMeta);
+                largeVeinOres.add(vein.mSporadicMeta);
+            }
         }
 
         if(isBartworksInstalled()) {
@@ -29,7 +39,14 @@ public class VPVeinCaching implements Runnable {
                                                 getBWOreVeinPrimaryMeta(vein),
                                                 getBWOreVeinSecondaryMeta(vein),
                                                 getBWOreVeinInBetweenMeta(vein),
-                                                getBWOreVeinSporadicMeta((vein))));
+                                                getBWOreVeinSporadicMeta(vein)));
+                if(getBWOreVeinSize(vein) > 16) {
+                    // These veins can be larger then 3x3 chunks. For these ores we need to check multiple possible locations
+                    largeVeinOres.add(getBWOreVeinPrimaryMeta(vein));
+                    largeVeinOres.add(getBWOreVeinSecondaryMeta(vein));
+                    largeVeinOres.add(getBWOreVeinInBetweenMeta(vein));
+                    largeVeinOres.add(getBWOreVeinSporadicMeta(vein));
+                }
             }
         }
 
