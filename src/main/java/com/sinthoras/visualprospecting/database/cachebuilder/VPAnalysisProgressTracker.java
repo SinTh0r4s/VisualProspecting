c5531c0144a0ccb454a8d1dca8537c32f4dae63d
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-02 00:57:23 +0200

    Collect user output and add timer

--- src/main/java/com/sinthoras/visualprospecting/database/cachebuilder/VPAnalysisProgressTracker.java
+++ src/main/java/com/sinthoras/visualprospecting/database/cachebuilder/VPAnalysisProgressTracker.java
@@ -3,6 +3,8 @@ package com.sinthoras.visualprospecting.database.cachebuilder;
 import com.sinthoras.visualprospecting.VP;
 import com.sinthoras.visualprospecting.VPConfig;
 
+import java.text.DecimalFormat;
+
 public class VPAnalysisProgressTracker {
 
     private static int numberOfDimensions = 0;
@@ -10,8 +12,10 @@ public class VPAnalysisProgressTracker {
     private static int numberOfRegionFiles = 0;
     private static int regionFilesProcessed = 0;
     private static long lastLogUpdate = 0;
+    private static long timestampMS = 0;
 
     public static void setNumberOfDimensions(int numberOfDimensions) {
+        timestampMS = System.currentTimeMillis();
         VPAnalysisProgressTracker.numberOfDimensions = numberOfDimensions;
         dimensionsProcessed = 0;
         updateLog();
@@ -41,5 +45,13 @@ public class VPAnalysisProgressTracker {
                     + (dimensionsProcessed + 1) + "/" + numberOfDimensions + ")  "
                     + (numberOfRegionFiles == 0 ? 0 : ((regionFilesProcessed * 100) / (numberOfRegionFiles * 100))) + "%");
         }
+    }
+
+    public static void processingFinished() {
+        final long elapsedTimeMS = System.currentTimeMillis() - timestampMS;
+        DecimalFormat format = new DecimalFormat();
+        format.setMinimumFractionDigits(1);
+        format.setMaximumFractionDigits(1);
+        VP.info("Parsing complete! Thank you for your patience.  - Duration: " + format.format(elapsedTimeMS / 1000) + "sec");
     }
 }
