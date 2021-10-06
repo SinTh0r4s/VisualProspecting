90013305cf84b82acb9449c1687e65d7647447f3
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-06 13:27:04 +0200

    Added injection into JourneyMap client data loading

--- src/main/java/com/sinthoras/visualprospecting/VPTags.java
+++ src/main/java/com/sinthoras/visualprospecting/VPTags.java
@@ -5,4 +5,6 @@ public class VPTags {
     public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String VISUALPROSPECTING = "VisualProspecting";
     public static final String COM_SINTHORAS_VISUALPROSPECTING = "com.sinthoras.visualprospecting";
+    public static final String SP_DATA_DIR = MODID + "\\data\\sp\\";
+    public static final String MP_DATA_DIR = MODID + "\\data\\mp\\";
 }
