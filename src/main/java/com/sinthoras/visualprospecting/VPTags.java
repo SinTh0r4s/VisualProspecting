12c051639a35c3404c8cb26ec197c699e97c4d92
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-08 12:10:33 +0200

    Move custom save files out of journeymap dir

--- src/main/java/com/sinthoras/visualprospecting/VPTags.java
+++ src/main/java/com/sinthoras/visualprospecting/VPTags.java
@@ -1,14 +1,12 @@
 package com.sinthoras.visualprospecting;
 
-import journeymap.client.Constants;
-
 public class VPTags {
 
     public static final String MODID = "visualprospecting";
     public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String VISUALPROSPECTING = "VisualProspecting";
     public static final String COM_SINTHORAS_VISUALPROSPECTING = "com.sinthoras.visualprospecting";
-    public static final String VISUALPROSPECTING_DIR = Constants.JOURNEYMAP_DIR + "\\" + MODID + "\\";
+    public static final String VISUALPROSPECTING_DIR = MODID + "\\";
     public static final String CLIENT_SP_DIR = VISUALPROSPECTING_DIR + "client\\sp\\";
     public static final String CLIENT_MP_DIR = VISUALPROSPECTING_DIR + "client\\mp\\";
     public static final String SERVER_DIR = VISUALPROSPECTING_DIR + "server\\";
