f32fefec67585ef0bda2e630073b469b60c82c91
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-08 15:01:12 +0200

    Missing in last

--- src/main/java/com/sinthoras/visualprospecting/VPTags.java
+++ src/main/java/com/sinthoras/visualprospecting/VPTags.java
@@ -7,8 +7,7 @@ public class VPTags {
     public static final String VISUALPROSPECTING = "VisualProspecting";
     public static final String COM_SINTHORAS_VISUALPROSPECTING = "com.sinthoras.visualprospecting";
     public static final String VISUALPROSPECTING_DIR = MODID + "\\";
-    public static final String CLIENT_SP_DIR = VISUALPROSPECTING_DIR + "client\\sp\\";
-    public static final String CLIENT_MP_DIR = VISUALPROSPECTING_DIR + "client\\mp\\";
+    public static final String CLIENT_DIR = VISUALPROSPECTING_DIR + "client\\";
     public static final String SERVER_DIR = VISUALPROSPECTING_DIR + "server\\";
     public static final String worldId = "wId";
 }
