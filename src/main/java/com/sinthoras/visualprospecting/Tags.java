8a32dacda0f59f4c5b37862bfc657b78ba6c77e2
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-14 00:51:01 +0200

    Fix folder names for linux

--- src/main/java/com/sinthoras/visualprospecting/Tags.java
+++ src/main/java/com/sinthoras/visualprospecting/Tags.java
@@ -6,11 +6,11 @@ public class Tags {
     public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String VISUALPROSPECTING = "VisualProspecting";
     public static final String COM_SINTHORAS_VISUALPROSPECTING = "com.sinthoras.visualprospecting";
-    public static final String VISUALPROSPECTING_DIR = MODID + "\\";
-    public static final String CLIENT_DIR = VISUALPROSPECTING_DIR + "client\\";
-    public static final String SERVER_DIR = VISUALPROSPECTING_DIR + "server\\";
-    public static final String UNDERGROUNDFLUID_DIR = "fluids\\";
-    public static final String OREVEIN_DIR = "ore\\";
+    public static final String VISUALPROSPECTING_DIR = MODID + "/";
+    public static final String CLIENT_DIR = VISUALPROSPECTING_DIR + "client/";
+    public static final String SERVER_DIR = VISUALPROSPECTING_DIR + "server/";
+    public static final String UNDERGROUNDFLUID_DIR = "fluids/";
+    public static final String OREVEIN_DIR = "ore/";
     public static final String worldId = "wId";
     public static final String ORE_MIX_NONE_NAME = "ore.mix.none";
     public static final String BOOK_TITLE = "title";
