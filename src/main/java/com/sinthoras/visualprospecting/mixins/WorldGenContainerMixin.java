388807325900925e42b4938b0b9aa5e99925ac94
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-16 20:31:23 +0200

    Fixed injection for gregtech ore vein notification

--- src/main/java/com/sinthoras/visualprospecting/mixins/WorldGenContainerMixin.java
+++ src/main/java/com/sinthoras/visualprospecting/mixins/WorldGenContainerMixin.java
@@ -1,62 +1,34 @@
 package com.sinthoras.visualprospecting.mixins;
 
+import com.sinthoras.visualprospecting.Utils;
 import com.sinthoras.visualprospecting.VP;
-import com.sinthoras.visualprospecting.database.veintypes.VeinType;
 import com.sinthoras.visualprospecting.database.veintypes.VeinTypeCaching;
-import gregtech.api.objects.XSTR;
 import gregtech.common.GT_Worldgen_GT_Ore_Layer;
 import gregtech.common.GT_Worldgenerator;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.chunk.IChunkProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Iterator;
+import java.util.Random;
 
-@Mixin(value = GT_Worldgenerator.WorldGenContainer.class, remap = false)
+@Mixin(GT_Worldgenerator.WorldGenContainer.class)
 public class WorldGenContainerMixin {
 
-    @Final
-    @Shadow(remap = false)
-    public int mX;
-
-    @Final
-    @Shadow(remap = false)
-    public int mZ;
-
-    @Final
-    @Shadow(remap = false)
-    public World mWorld;
-
-    @Inject(method = "worldGenFindVein",
-            at = @At(value = "INVOKE", target = "Ljava/util/Hashtable;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
-            remap = false,
-            require = 1,
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onOreVeinPlacedFailedA(int oreseedX, int oreseedZ, CallbackInfo callbackInfo, long oreveinSeed, XSTR oreveinRNG, int oreveinPercentageRoll, int noOrePlacedCount, String tDimensionName) {
-        VP.serverCache.putOreVein(mWorld.provider.dimensionId, mX, mZ, VeinType.NO_VEIN);
+    // Redirect both call to ensure that Bartworks ore veins are captured as well
+    @Redirect(method = "worldGenFindVein",
+            at = @At(value = "INVOKE", target = "Lgregtech/common/GT_Worldgen_GT_Ore_Layer;executeWorldgenChunkified(Lnet/minecraft/world/World;Ljava/util/Random;Ljava/lang/String;IIIIILnet/minecraft/world/chunk/IChunkProvider;Lnet/minecraft/world/chunk/IChunkProvider;)I"),
+            remap = false)
+    protected int onOreVeinPlaced(GT_Worldgen_GT_Ore_Layer instance, World aWorld, Random aRandom, String aBiome, int aDimensionType, int aChunkX, int aChunkZ, int aSeedX, int aSeedZ, IChunkProvider aChunkGenerator, IChunkProvider aChunkProvider) {
+        final int result = instance.executeWorldgenChunkified(aWorld, aRandom, aBiome, aDimensionType, aChunkX, aChunkZ, aSeedX, aSeedZ, aChunkGenerator, aChunkProvider);
+        if(result == GT_Worldgen_GT_Ore_Layer.ORE_PLACED && instance.mWorldGenName.equals("NoOresInVein") == false) {
+            VP.serverCache.putOreVein(
+                    aWorld.provider.dimensionId,
+                    Utils.coordBlockToChunk(aSeedX),
+                    Utils.coordBlockToChunk(aSeedZ),
+                    VeinTypeCaching.getVeinType(instance.mWorldGenName));
+        }
+        return result;
     }
-
-    @Inject(method = "worldGenFindVein",
-            at = @At(value = "INVOKE", target = "Ljava/util/Hashtable;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
-            remap = false,
-            require = 1,
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onOreVeinPlacedFailedB(int oreseedX, int oreseedZ, CallbackInfo ci, long oreveinSeed, XSTR oreveinRNG, int oreveinPercentageRoll, int noOrePlacedCount, String tDimensionName, int placementAttempts, boolean oreveinFound, int i) {
-        VP.serverCache.putOreVein(mWorld.provider.dimensionId, mX, mZ, VeinType.NO_VEIN);
-    }
-
-    @Inject(method = "worldGenFindVein",
-            at = @At(value = "INVOKE", target = "Ljava/util/Hashtable;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
-            remap = false,
-            require = 2,
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onOreVeinPlacedSuccess(int oreseedX, int oreseedZ, CallbackInfo callbackInfo, long oreveinSeed, XSTR oreveinRNG, int oreveinPercentageRoll, int noOrePlacedCount, String tDimensionName, int placementAttempts, boolean oreveinFound, int i, int tRandomWeight, Iterator var13, GT_Worldgen_GT_Ore_Layer tWorldGen, int placementResult) {
-        VP.serverCache.putOreVein(mWorld.provider.dimensionId, mX, mZ, VeinTypeCaching.getVeinType(tWorldGen.mWorldGenName));
-    }
-
 }
