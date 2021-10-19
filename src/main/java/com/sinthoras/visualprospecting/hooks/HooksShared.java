a839c45f86e85cdf79da8a23fb7b8b1625a53cf8
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-19 17:54:36 +0200

    Add recipe for Prospector's Log

--- src/main/java/com/sinthoras/visualprospecting/hooks/HooksShared.java
+++ src/main/java/com/sinthoras/visualprospecting/hooks/HooksShared.java
@@ -24,6 +24,12 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import gregtech.api.GregTech_API;
+import gregtech.api.enums.GT_Values;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.util.GT_OreDictUnificator;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -59,6 +65,16 @@ public class HooksShared {
 	// postInit "Handle interaction with other mods, complete your setup based on this."
 	public void fmlLifeCycleEvent(FMLPostInitializationEvent event) {
 		GregTech_API.sAfterGTPostload.add(new VeinTypeCaching());
+		GregTech_API.sAfterGTPostload.add(() -> GT_Values.RA.addAssemblerRecipe(
+				new ItemStack[] {
+						GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Wood, 2L),
+						new ItemStack(Items.writable_book, 1, 0x7FFF),
+						new ItemStack(Items.gold_nugget, 1, 0x7FFF)
+				},
+				Materials.Glue.getFluid(20L),
+				new ItemStack(ProspectorsLog.instance, 1, 0),
+				128,
+				8));
 	}
 	
 	public void fmlLifeCycleEvent(FMLServerAboutToStartEvent event) {
