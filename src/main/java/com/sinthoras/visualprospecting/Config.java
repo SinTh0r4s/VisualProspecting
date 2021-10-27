a21e80a976ea05894f5b7bc8907307bd0018e21e
Johann Bernhardt <johann.bernhardt@tum.de>
2021-10-27 13:52:19 +0200

    Specified side for config parameters

--- src/main/java/com/sinthoras/visualprospecting/Config.java
+++ src/main/java/com/sinthoras/visualprospecting/Config.java
@@ -40,35 +40,35 @@ public class Config {
         configuration.load();
 
         Property enableProspectingProperty = configuration.get(Categories.general, "enableProspecting",
-                Defaults.enableProspecting, "You may want to disable prospecting for low-performance clients.");
+                Defaults.enableProspecting, "[CLIENT] You may want to disable prospecting for low-performance clients.");
         enableProspecting = enableProspectingProperty.getBoolean();
 
         Property cacheGenerationLogUpdateMinTimeProperty = configuration.get(Categories.general, "cacheGenerationLogUpdateMinTime",
-                Defaults.cacheGenerationLogUpdateMinTime, "Minimum between log updates to show progress when " +
+                Defaults.cacheGenerationLogUpdateMinTime, "[SERVER] Minimum between log updates to show progress when " +
                         "caching save files. This happens only ONCE!");
         cacheGenerationLogUpdateMinTime = cacheGenerationLogUpdateMinTimeProperty.getInt();
 
         Property minDelayBetweenVeinRequestsProperty = configuration.get(Categories.network, "minDelayBetweenVeinRequests",
-                Defaults.minDelayBetweenVeinRequests, "Anti spam mechanic: What is the minimum delay (in milliseconds)" +
+                Defaults.minDelayBetweenVeinRequests, "[CLIENT + SERVER] Anti spam mechanic: What is the minimum delay (in milliseconds)" +
                         " a player is allowed to request ore vein information.");
         minDelayBetweenVeinRequests = minDelayBetweenVeinRequestsProperty.getInt();
 
         Property minZoomLevelProperty = configuration.get(Categories.general, "minZoomLevelForOreLabel", Defaults.minZoomLevelForOreLabel,
-                "Minimum zoom level at which ore veins labels are displayed. Zoom starts at 0 and increments linearly.");
+                "[CLIENT] Minimum zoom level at which ore veins labels are displayed. Zoom starts at 0 and increments linearly.");
         minZoomLevelForOreLabel = minZoomLevelProperty.getInt();
 
         Property minZoomLevelForUndergroundFluidDetailsProperty = configuration.get(Categories.general, "minZoomLevelForUndergroundFluidDetails",
-                Defaults.minZoomLevelForUndergroundFluidDetails, "\"Minimum zoom level at which underground fluid details are displayed. Zoom starts at 0 and increments linearly.\"");
+                Defaults.minZoomLevelForUndergroundFluidDetails, "[CLIENT] Minimum zoom level at which underground fluid details are displayed. Zoom starts at 0 and increments linearly.");
         minZoomLevelForUndergroundFluidDetails = minZoomLevelForUndergroundFluidDetailsProperty.getInt();
 
         Property uploadBandwidthProperty = configuration.get(Categories.network, "uploadBandwidth", Defaults.uploadBandwidthBytes,
-                "Limit the bandwidth (in B/s) a client is allowed to transmit when uploading its prospection data." +
+                "[CLIENT + SERVER] Limit the bandwidth (in B/s) a client is allowed to transmit when uploading its prospection data." +
                         " If exceeded, the client will be kicked!");
         uploadBandwidthBytes = uploadBandwidthProperty.getDouble();
         uploadSizePerPacket = (int)(uploadBandwidthBytes / uploadPacketsPerSecond);
 
         Property recacheVeinsProperty = configuration.get(Categories.general, "recacheVeins", Defaults.recacheVeins,
-                "Redo GT ore vein caching if set to True. Will automatically be set back to False the next " +
+                "[SERVER] Redo GT ore vein caching if set to True. Will automatically be set back to False the next " +
                         "time the game is started.");
         recacheVeins = recacheVeinsProperty.getBoolean();
         if(recacheVeins) {
