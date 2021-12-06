5b6fb5476be0ba402b6e6aab4c61e55f95f2db09
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-06 14:44:33 +0100

    First draft of concurrent world scanning

--- src/main/java/com/sinthoras/visualprospecting/database/cachebuilder/DimensionAnalysis.java
+++ src/main/java/com/sinthoras/visualprospecting/database/cachebuilder/DimensionAnalysis.java
@@ -12,61 +12,68 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.zip.DataFormatException;
 
 public class DimensionAnalysis {
 
     public final int dimensionId;
-    private final Map<Long, DetailedChunkAnalysis> chunksForSecondIdentificationPass = new HashMap<>();
+    private final Map<Long, DetailedChunkAnalysis> chunksForSecondIdentificationPass = new ConcurrentHashMap<>();
 
     public DimensionAnalysis(int dimensionId) {
         this.dimensionId = dimensionId;
     }
 
     public void processMinecraftWorld(MinecraftWorld world) throws IOException, DataFormatException {
-        final Map<Long, Integer> veinBlockY = new HashMap<>();
+        final Map<Long, Integer> veinBlockY = new ConcurrentHashMap<>();
         final List<File> regionFiles = world.getAllRegionFiles(dimensionId);
         AnalysisProgressTracker.setNumberOfRegionFiles(regionFiles.size());
-        for (File regionFile : regionFiles) {
-            final String[] parts = regionFile.getName().split("\\.");
-            final int regionChunkX = Integer.parseInt(parts[1]) << 5;
-            final int regionChunkZ = Integer.parseInt(parts[2]) << 5;
-            final MinecraftRegion region = new MinecraftRegion(regionFile);
-            for (int localChunkX = 0; localChunkX < VP.chunksPerRegionFileX; localChunkX++) {
-                for (int localChunkZ = 0; localChunkZ < VP.chunksPerRegionFileZ; localChunkZ++) {
-                    final int chunkX = regionChunkX + localChunkX;
-                    final int chunkZ = regionChunkZ + localChunkZ;
 
-                    // Only process ore chunks
-                    if (chunkX == Utils.mapToCenterOreChunkCoord(chunkX) && chunkZ == Utils.mapToCenterOreChunkCoord(chunkZ)) {
-                        // Helpful read about 'root' structure: https://minecraft.fandom.com/wiki/Chunk_format
-                        final NBTCompound root = region.getChunk(localChunkX, localChunkZ).getRootTag();
+        regionFiles.parallelStream().forEach(regionFile -> {
+            try {
+                final String[] parts = regionFile.getName().split("\\.");
+                final int regionChunkX = Integer.parseInt(parts[1]) << 5;
+                final int regionChunkZ = Integer.parseInt(parts[2]) << 5;
+                final MinecraftRegion region = new MinecraftRegion(regionFile);
+                for (int localChunkX = 0; localChunkX < VP.chunksPerRegionFileX; localChunkX++) {
+                    for (int localChunkZ = 0; localChunkZ < VP.chunksPerRegionFileZ; localChunkZ++) {
+                        final int chunkX = regionChunkX + localChunkX;
+                        final int chunkZ = regionChunkZ + localChunkZ;
 
-                        // root == null occurs when a chunk is not yet generated
-                        if (root != null) {
-                            final ChunkAnalysis chunk = new ChunkAnalysis();
-                            chunk.processMinecraftChunk(root);
+                        // Only process ore chunks
+                        if (chunkX == Utils.mapToCenterOreChunkCoord(chunkX) && chunkZ == Utils.mapToCenterOreChunkCoord(chunkZ)) {
+                            // Helpful read about 'root' structure: https://minecraft.fandom.com/wiki/Chunk_format
+                            final NBTCompound root = region.getChunk(localChunkX, localChunkZ).getRootTag();
 
-                            if (chunk.matchesSingleVein()) {
-                                ServerCache.instance.notifyOreVeinGeneration(dimensionId, chunkX, chunkZ, chunk.getMatchedVein());
-                                veinBlockY.put(Utils.chunkCoordsToKey(chunkX, chunkZ), chunk.getVeinBlockY());
-                            } else {
-                                final DetailedChunkAnalysis detailedChunk = new DetailedChunkAnalysis(dimensionId, chunkX, chunkZ);
-                                detailedChunk.processMinecraftChunk(root);
-                                chunksForSecondIdentificationPass.put(Utils.chunkCoordsToKey(chunkX, chunkZ), detailedChunk);
+                            // root == null occurs when a chunk is not yet generated
+                            if (root != null) {
+                                final ChunkAnalysis chunk = new ChunkAnalysis();
+                                chunk.processMinecraftChunk(root);
+
+                                if (chunk.matchesSingleVein()) {
+                                    ServerCache.instance.notifyOreVeinGeneration(dimensionId, chunkX, chunkZ, chunk.getMatchedVein());
+                                    veinBlockY.put(Utils.chunkCoordsToKey(chunkX, chunkZ), chunk.getVeinBlockY());
+                                } else {
+                                    final DetailedChunkAnalysis detailedChunk = new DetailedChunkAnalysis(dimensionId, chunkX, chunkZ);
+                                    detailedChunk.processMinecraftChunk(root);
+                                    chunksForSecondIdentificationPass.put(Utils.chunkCoordsToKey(chunkX, chunkZ), detailedChunk);
+                                }
                             }
                         }
                     }
                 }
+                region.close();
+                AnalysisProgressTracker.regionFileProcessed();
             }
-            region.close();
-            AnalysisProgressTracker.regionFileProcessed();
-        }
+            catch (DataFormatException | IOException e) {
+                AnalysisProgressTracker.notifyCorruptFile(regionFile);
+            }
+        });
 
-        for(long key : chunksForSecondIdentificationPass.keySet()) {
+        chunksForSecondIdentificationPass.keySet().parallelStream().forEach(key -> {
             final DetailedChunkAnalysis chunk = chunksForSecondIdentificationPass.get(key);
             chunk.cleanUpWithNeighbors(veinBlockY);
             ServerCache.instance.notifyOreVeinGeneration(dimensionId, chunk.chunkX, chunk.chunkZ, chunk.getMatchedVein());
-        }
+        });
     }
 }
