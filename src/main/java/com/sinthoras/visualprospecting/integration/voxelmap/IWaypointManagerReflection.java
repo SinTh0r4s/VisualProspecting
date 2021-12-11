4604af9dccda7c0fa65399ee41a7564927433d6f
glowredman <fredcraft00@gmail.com>
2021-12-11 20:28:05 +0100

    Initialize IWaypointManagerReflection.getCurrentSubworldDescriptor on clinit

--- src/main/java/com/sinthoras/visualprospecting/integration/voxelmap/IWaypointManagerReflection.java
+++ src/main/java/com/sinthoras/visualprospecting/integration/voxelmap/IWaypointManagerReflection.java
@@ -7,17 +7,22 @@ import com.thevoxelbox.voxelmap.interfaces.IWaypointManager;
 public class IWaypointManagerReflection {
 
 	private static Method getCurrentSubworldDescriptor;
-	
+
 	public static String getCurrentSubworldDescriptor(IWaypointManager obj, boolean arg) {
 		try {
-			if(getCurrentSubworldDescriptor == null) {
-				getCurrentSubworldDescriptor = IWaypointManager.class.getMethod("if", boolean.class);
-			}
 			return (String) getCurrentSubworldDescriptor.invoke(obj, arg);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "";
+	}
+
+	static {
+		try {
+			getCurrentSubworldDescriptor = IWaypointManager.class.getMethod("if", boolean.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
