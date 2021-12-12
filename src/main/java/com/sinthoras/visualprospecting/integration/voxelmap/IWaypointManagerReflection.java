78828136eb7fa2b01648b991acc3a3afa99b1a78
glowredman <fredcraft00@gmail.com>
2021-12-12 17:47:21 +0100

    Add error messages in IWaypointManagerReflection

--- src/main/java/com/sinthoras/visualprospecting/integration/voxelmap/IWaypointManagerReflection.java
+++ src/main/java/com/sinthoras/visualprospecting/integration/voxelmap/IWaypointManagerReflection.java
@@ -2,6 +2,7 @@ package com.sinthoras.visualprospecting.integration.voxelmap;
 
 import java.lang.reflect.Method;
 
+import com.sinthoras.visualprospecting.VP;
 import com.thevoxelbox.voxelmap.interfaces.IWaypointManager;
 
 public class IWaypointManagerReflection {
@@ -12,6 +13,7 @@ public class IWaypointManagerReflection {
 		try {
 			return (String) getCurrentSubworldDescriptor.invoke(obj, arg);
 		} catch (Exception e) {
+			VP.error("Could not invoke IWaypointManager#if. If it failed due to a NullPointerException, look for an error message starting with \"Getting the method IWaypointManager#if failed\" further up.");
 			e.printStackTrace();
 		}
 		return "";
@@ -21,6 +23,7 @@ public class IWaypointManagerReflection {
 		try {
 			getCurrentSubworldDescriptor = IWaypointManager.class.getMethod("if", boolean.class);
 		} catch (Exception e) {
+			VP.error("Getting the method IWaypointManager#if failed, any calls to IWaypointManagerReflection#getCurrentSubworldDescriptor will return an empty String.");
 			e.printStackTrace();
 		}
 	}
