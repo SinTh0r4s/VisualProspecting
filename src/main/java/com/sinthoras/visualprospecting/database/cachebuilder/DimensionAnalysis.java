6cba11e37bcb2194d1597662483cfc7a84be6bd3
bombcar <github@bombcar.com>
2021-12-24 20:20:55 -0600

    use regex instead

--- src/main/java/com/sinthoras/visualprospecting/database/cachebuilder/DimensionAnalysis.java
+++ src/main/java/com/sinthoras/visualprospecting/database/cachebuilder/DimensionAnalysis.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.DataFormatException;
 
 public class DimensionAnalysis {
@@ -90,7 +92,7 @@ public class DimensionAnalysis {
 
     private void executeForEachGeneratedOreChunk(File regionFile, IChunkHandler chunkHandler) {
         try {
-            if ( regionFile.getName().contains("(")) {
+            if ( !Pattern.matches("^r\\.-?\\d+\\.-?\\d+.mca$", regionFile.getName())) {
                 VP.warn("Invalid region file found! " + regionFile.getName() + " continuing");
                 return;
             }
